bcf16a24a1cd2cae2b5c3e00446ee886ce5de194
Markus Schaber <m.schaber@codesys.com>
2020-08-10 15:16:33 +0200

    Push quick and dirty code example of what does not work.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ConsoleApp1
+{
+    using System;
+
+    using TestNugetPackage;
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.TestString);
+        }
+    }
+}
---
+++ TestNugetPackage/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TestNugetPackage
+{
+    public static class Class1
+    {
+        public static string TestString => Strings.TestText;
+    }
+}
